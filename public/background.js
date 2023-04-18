07f2f4876b61316e50072ab8e8975e76346f66a9
adarshkunwar <hellboyadarsh18@gmail.com>
2023-04-18 12:39:37 +0545

    its center aligned now

--- public/background.js
+++ public/background.js
@@ -1,13 +1,33 @@
 chrome.action.onClicked.addListener((tab) => {
-  chrome.windows.create(
-    {
-      url: "../index.html",
-      type: "popup",
-      width: 800,
-      height: 600,
-    },
-    (window) => {
-      console.log("Window opened:", window);
+  // Define variables for the window properties
+  let redirect = "../index.html";
+  let type = "popup";
+  let width = 800;
+  let height = 600;
+
+  // Get the display info and calculate the center of the screen
+  chrome.system.display.getInfo((displayInfo) => {
+    if (displayInfo.length > 0) {
+      let primaryDisplay = displayInfo.find((display) => display.isPrimary);
+      let screenLeft = Math.round(
+        primaryDisplay.bounds.left + (primaryDisplay.bounds.width - width) / 2
+      );
+      let screenTop = Math.round(
+        primaryDisplay.bounds.top + (primaryDisplay.bounds.height - height) / 2
+      );
+
+      // Create the new window at the center of the screen
+      chrome.windows.create({
+        url: redirect,
+        type: type,
+        width: width,
+        height: height,
+        left: screenLeft,
+        top: screenTop,
+      });
+    } else {
+      // Handle error - no displays found
+      console.error("No displays found");
     }
-  );
+  });
 });
