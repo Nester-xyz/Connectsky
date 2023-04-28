7f4115d3b19d505e6fc7f2a1b1fc670dd8a117b1
whoisanku <newmew.ankit@gmail.com>
2023-04-28 10:54:25 +0545

    js file to ts file

---
+++ tailwind.config.ts
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        gray: "#f0f2f5",
+      },
+    },
+    plugins: [],
+  },
+};
