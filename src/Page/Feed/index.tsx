4501668b0e0193e1d14452365bd77f93402ad56f
Aryog <yogesh000aryal@gmail.com>
2023-04-30 09:26:33 +0545

    infinite scroll handled

--- src/Page/Feed/index.tsx
+++ src/Page/Feed/index.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import { dummyData } from "./dummyData";
 import PostSection from "../../components/PageComponents/Feed/PostSection";
 import PostCard from "../../components/PageComponents/Feed/PostCard";
@@ -13,171 +13,208 @@ import { BskyAgent, AtpSessionData, AtpSessionEvent } from "@atproto/api";
 //   comments: number;
 // };
 interface Author {
-  displayName: string;
-  avatar: string;
+    displayName: string;
+    avatar: string;
 }
 
 interface Caption {
-  text: string;
+    text: string;
 }
 
 interface Image {
-  thumb: string;
+    thumb: string;
 }
 
 interface Embed {
-  images: Image[];
+    images: Image[];
 }
 
 interface ImageObject {
-  embed: Embed;
+    embed: Embed;
 }
 
 interface Item {
-  author: Author;
-  comments: number;
-  likes: number;
-  caption: Caption;
-  image: ImageObject;
+    author: Author;
+    comments: number;
+    likes: number;
+    caption: Caption;
+    image: ImageObject;
 }
 
 const differentButtonsForFeed = [
-  {
-    name: "Media",
-    icon: undefined,
-    action: () => { },
-  },
-  {
-    name: "Links",
-    icon: undefined,
-    action: () => { },
-  },
-  {
-    name: "GIF",
-    icon: undefined,
-    action: () => { },
-  },
-  {
-    name: "Post",
-    icon: undefined,
-    action: () => { },
-  },
+    {
+        name: "Media",
+        icon: undefined,
+        action: () => { },
+    },
+    {
+        name: "Links",
+        icon: undefined,
+        action: () => { },
+    },
+    {
+        name: "GIF",
+        icon: undefined,
+        action: () => { },
+    },
+    {
+        name: "Post",
+        icon: undefined,
+        action: () => { },
+    },
 ];
 
 const feedOptionsButtons = [
-  {
-    name: "For you",
-    filter: "forYou",
-    icon: undefined,
-  },
-  {
-    name: "Following",
-    filter: "following",
-    icon: undefined,
-  },
+    {
+        name: "For you",
+        filter: "forYou",
+        icon: undefined,
+    },
+    {
+        name: "Following",
+        filter: "following",
+        icon: undefined,
+    },
 ];
 
 const Feed = () => {
-  const [filterVariable, setFilterVariable] = useState<string>("forYou");
-  const [cursor, setCursor] = useState<string>("");
-  const [feedData, setFeedData] = useState<Item[]>([]);
+    const [filterVariable, setFilterVariable] = useState<string>("forYou");
+    const [cursor, setCursor] = useState<string>("");
+    const [isLoading, setIsLoading] = useState<boolean>(false);
+    const [feedData, setFeedData] = useState<Item[]>([]);
+    const lastElementRef = useRef<HTMLDivElement | null>(null);
 
-  const agent = new BskyAgent({
-    service: "https://bsky.social",
-    persistSession: (_evt: AtpSessionEvent, sess?: AtpSessionData) => {
-      console.log("first");
-    },
-  });
+    const agent = new BskyAgent({
+        service: "https://bsky.social",
+        persistSession: (_evt: AtpSessionEvent, sess?: AtpSessionData) => {
+            console.log("first");
+        },
+    });
 
-  useEffect(() => {
     async function followingFeed() {
-      const sessData = localStorage.getItem("sess");
-      if (sessData !== null) {
-        const sessParse = JSON.parse(sessData);
-        await agent.resumeSession(sessParse);
-      }
-      const { data } = await agent.getTimeline({
-        limit: 20,
-        cursor: cursor,
-      });
-      if (data.cursor == null) return;
-      setCursor(data.cursor);
-      const mappedData: Item[] = data.feed.map((feed: any) => {
-        console.log(feed);
-        const images = feed.post.embed && 'images' in feed.post.embed ? feed.post.embed.images : [];
-        const firstImageThumb = images?.length > 0 ? images[0].thumb : '';
+        const sessData = localStorage.getItem("sess");
+        if (sessData !== null) {
+            const sessParse = JSON.parse(sessData);
+            await agent.resumeSession(sessParse);
+        }
+        const { data } = await agent.getTimeline({
+            limit: 20,
+            cursor: cursor,
+        });
+        if (data.cursor == null) return;
+        setCursor(data.cursor);
+        const mappedData: Item[] = data.feed.map((feed: any) => {
+            console.log(feed);
+            const images = feed.post.embed && 'images' in feed.post.embed ? feed.post.embed.images : [];
+            const firstImageThumb = images?.length > 0 ? images[0].thumb : '';
 
-        return {
-          author: {
-            displayName: feed.post.author.displayName,
-            avatar: feed.post.author.avatar,
-          },
-          likes: feed.post.likeCount,
-          comments: feed.post.replyCount,
-          caption: {
-            text: feed.post.record && "text" in feed.post.record ? feed.post.record.text : "",
-          },
-          image: {
-            embed: {
-              images: [{ thumb: firstImageThumb }]
-            }
-          }
-        };
-      });
-      setFeedData(mappedData);
+            return {
+                author: {
+                    displayName: feed.post.author.displayName,
+                    avatar: feed.post.author.avatar,
+                },
+                likes: feed.post.likeCount,
+                comments: feed.post.replyCount,
+                caption: {
+                    text: feed.post.record && "text" in feed.post.record ? feed.post.record.text : "",
+                },
+                image: {
+                    embed: {
+                        images: [{ thumb: firstImageThumb }]
+                    }
+                }
+            };
+        });
+        setFeedData((prevData) => [...prevData, ...mappedData]);
     }
-    followingFeed();
-  }, []);
-  console.log(feedData);
-  return (
-    <div className=" w-full  px-5">
-      {/* create the top - post option */}
+    const observerCallback: IntersectionObserverCallback = (entries) => {
+        const firstEntry = entries[0];
+        console.log(firstEntry);
+        if (firstEntry.isIntersecting) {
+            followingFeed();
+            setIsLoading(true);
+        }
+    };
+    const observer = new IntersectionObserver(observerCallback, { threshold: 0.5 });
 
-      <div className="grid grid-cols-4 gap-5 h-screen relative overflow-hidden">
-        <div className="col-span-4 md:col-span-3 overflow-scroll mt-5">
-          <div className="flex gap-3 sticky top-0 pb-2 bg-white">
-            {feedOptionsButtons.map((item, index) => {
-              return (
-                <div key={index}>
-                  <button
-                    className="bg-white border rounded-md border-gray-300 flex-1 md:w-48 min-w-[100px]"
-                    onClick={() => setFilterVariable(item.filter)}
-                  >
-                    {item.name}
-                  </button>
-                </div>
-              );
-            })}
-          </div>
+    useEffect(() => {
+        if (!isLoading) {
+            followingFeed();
+            setIsLoading(true);
+        }
+        if (lastElementRef.current) {
+            observer.observe(lastElementRef.current);
+        }
 
-          <PostSection differentButtonsForFeed={differentButtonsForFeed} />
-
-          {/* create the feed */}
-          <div className=" rounded-md  w-full flex flex-col gap-5 mt-5">
-            {
-              // here we will map the feed
-
-              feedData.map((item: Item, index) => {
-                return (
-                  <div>
-                    <PostCard
-                      author={item.author.displayName}
-                      comments={item.comments}
-                      likes={item.likes}
-                      caption={item.caption.text}
-                      image={item.image.embed.images[0].thumb}
-                      profileImg={item.author.avatar}
-                    />
-                  </div>
-                );
-              })
+        return () => {
+            if (lastElementRef.current) {
+                observer.unobserve(lastElementRef.current);
             }
-          </div>
+        };
+    }, [isLoading, observer]);
+    return (
+        <div className=" w-full  px-5">
+            {/* create the top - post option */}
+
+            <div className="grid grid-cols-4 gap-5 h-screen relative">
+                <div className="col-span-4 md:col-span-3 mt-5">
+                    <div className="flex gap-3 sticky top-0 pb-2 bg-white">
+                        {feedOptionsButtons.map((item, index) => {
+                            return (
+                                <div key={index}>
+                                    <button
+                                        className="bg-white border rounded-md border-gray-300 flex-1 md:w-48 min-w-[100px]"
+                                        onClick={() => setFilterVariable(item.filter)}
+                                    >
+                                        {item.name}
+                                    </button>
+                                </div>
+                            );
+                        })}
+                    </div>
+
+                    <PostSection differentButtonsForFeed={differentButtonsForFeed} />
+
+                    {/* create the feed */}
+                    <div className=" rounded-md  w-full flex flex-col gap-5 mt-5">
+                        {
+                            // here we will map the feed
+
+                            feedData.map((item: Item, index) => {
+                                if (index === feedData.length - 1) {
+                                    return (
+                                        <div ref={lastElementRef} key={index}>
+                                            <PostCard
+                                                author={item.author.displayName}
+                                                comments={item.comments}
+                                                likes={item.likes}
+                                                caption={item.caption.text}
+                                                image={item.image.embed.images[0].thumb}
+                                                profileImg={item.author.avatar}
+                                            />
+                                        </div>
+                                    );
+                                } else {
+                                    return <div key={index}>
+                                        <PostCard
+                                            author={item.author.displayName}
+                                            comments={item.comments}
+                                            likes={item.likes}
+                                            caption={item.caption.text}
+                                            image={item.image.embed.images[0].thumb}
+                                            profileImg={item.author.avatar}
+                                        />
+                                    </div>
+                                }
+                            })
+                        }
+                        {isLoading ? "Loading..." : ""}
+                    </div>
+                </div>
+                <div className="bg-slate-50 hidden md:block col-span-1 sticky h-full top-5"></div>
+            </div>
         </div>
-        <div className="bg-slate-50 hidden md:block col-span-1 sticky h-full top-5"></div>
-      </div>
-    </div>
-  );
+    );
 };
 
 export default Feed;
