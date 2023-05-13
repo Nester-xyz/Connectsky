82a352092a3bc0346f2f73a363f690757a216101
adarshkunwar <hellboyadarsh18@gmail.com>
2023-05-13 17:24:12 +0545

    profile

--- src/Page/Profile/index.tsx
+++ src/Page/Profile/index.tsx
@@ -1,9 +1,47 @@
 import React from "react";
+import PostLoader from "../../components/PageComponents/Feed/PostLoader";
 
 type Props = {};
 
 const index = (props: Props) => {
-  return <div>index</div>;
+  return (
+    <div className="p-5">
+      {/* cover */}
+
+      <div className="bg-slate-50 w-full h-56 relative">
+        {/* <img src="" alt="" /> //cover image */}
+        <div className="px-5 py-1 border rounded-md absolute right-5 top-5">
+          Follow
+        </div>
+        {/* profile */}
+        <div className="w-32 bg-blue-300 aspect-square border rounded-full absolute left-10 -bottom-16"></div>
+      </div>
+
+      {/* profile details */}
+      <div className="flex flex-col gap-3 mt-16">
+        <div className="flex flex-col gap-1">
+          <div className="text-2xl font-bold">Name</div>
+          <div className="text-sm text-gray-500">@handle</div>
+        </div>
+        <div className="flex flex-col gap-1">
+          <div className="text-sm text-gray-500">Bio</div>
+          <div className="text-sm text-gray-500">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam
+            voluptatum, quibusdam, quia, quod voluptates voluptatem quos
+            voluptate quas doloribus quidem voluptatibus. Quisquam voluptatum,
+          </div>
+        </div>
+      </div>
+
+      {/* posts */}
+
+      <div className="mt-5 flex flex-col gap-3">
+        <PostLoader />
+        <PostLoader />
+        <PostLoader />
+      </div>
+    </div>
+  );
 };
 
 export default index;
--- src/components/PageComponents/Feed/TextAreaBox.tsx
+++ src/components/PageComponents/Feed/TextAreaBox.tsx
@@ -1,7 +1,5 @@
-import React, { useState, useContext, useEffect } from "react";
-
+import React, { useState, useContext, useEffect, useRef } from "react";
 import { appContext } from "../../../context/appContext";
-
 import { CiFaceSmile } from "react-icons/ci";
 import EmojiPicker from "emoji-picker-react";
 import { useDebounce } from "use-debounce";
@@ -13,12 +11,14 @@ type Props = {
 
 const TextAreaBox = ({ showImage, imgUpload }: Props) => {
   const { postText, setPostText } = useContext(appContext);
-  // this is just a local declaration. It will change in the future
+
+  const textBox = useRef<HTMLTextAreaElement>(null);
+  const emoji = useRef<HTMLDivElement>(null);
+
   const [postTextLocal, setPostTextLocal] = useState("");
-
   const [debouncedPostText] = useDebounce(postTextLocal, 1000);
-
   const [showEmoji, setShowEmoji] = useState(false);
+  const [cursorPosition, setCursorPosition] = useState(0);
 
   useEffect(() => {
     setPostText(debouncedPostText);
@@ -26,6 +26,7 @@ const TextAreaBox = ({ showImage, imgUpload }: Props) => {
 
   const handlePost = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
     setPostText(e.target.value);
+    setCursorPosition(e.target.selectionStart);
   };
 
   const onEmojiClick = (emojiObject: any) => {
@@ -33,16 +34,38 @@ const TextAreaBox = ({ showImage, imgUpload }: Props) => {
     setShowEmoji(false);
   };
 
+  useEffect(() => {
+    if (showEmoji && textBox.current && emoji.current) {
+      const textBoxRect = textBox.current.getBoundingClientRect();
+      const { style } = emoji.current;
+
+      console.log(textBoxRect.left, textBoxRect.top, textBoxRect.height);
+
+      style.position = "absolute";
+      style.left = `${textBoxRect.left + textBox.current.clientLeft}px`;
+      style.top = `${
+        textBoxRect.top + textBoxRect.height + textBox.current.clientTop
+      }px`;
+    }
+  }, [showEmoji, cursorPosition]);
+
   return (
     <div className="flex flex-col items-center relative">
       <textarea
+        ref={textBox}
         name="post-textarea"
         id="post-textarea"
         className="w-full h-56 resize-none px-4 py-2 rounded-xl focus:outline-none"
         onChange={handlePost}
         value={postText}
+        onClick={(e) => {
+          const target = e.target as HTMLTextAreaElement;
+          setCursorPosition(target.selectionStart);
+        }}
       ></textarea>
-      <div>{showEmoji && <EmojiPicker onEmojiClick={onEmojiClick} />}</div>
+      <div className="absolute mt-5 ml-5" ref={emoji}>
+        {showEmoji && <EmojiPicker onEmojiClick={onEmojiClick} />}
+      </div>
       <div
         className="rounded-full w-8 h-8 absolute right-2 bottom-2"
         onClick={() => {
@@ -51,10 +74,7 @@ const TextAreaBox = ({ showImage, imgUpload }: Props) => {
       >
         <CiFaceSmile className="w-full h-full" />
       </div>
-      <div
-        className="bg-blue-300 rounded-md w-8 h-8 absolute
-       left-2 bottom-2"
-      ></div>
+      <div className="bg-blue-300 rounded-md w-8 h-8 absolute left-2 bottom-2"></div>
       {showImage && (
         <div className="border w-40 h-full overflow-hidden items-center">
           <img src={imgUpload} alt="broken" className="object-contain w-40" />
