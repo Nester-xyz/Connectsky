e7e0672c3814ebd755da8a18387bcb23add12104
Aryog <yogesh000aryal@gmail.com>
2023-05-15 22:52:46 +0545

    embed bug

--- src/components/PageComponents/Feed/PostCard.tsx
+++ src/components/PageComponents/Feed/PostCard.tsx
@@ -242,7 +242,7 @@ const PostCard = ({
             <div className="text-base">{embed?.data?.value?.text}</div>
             <div>
               {/* section for image if available; */}
-              {embed?.data?.embed && <img src={embed?.data?.embeds[0]?.images[0]?.thumb} alt="" />}
+              {embed.data?.embeds[0]?.images && <img src={embed?.data?.embeds[0]?.images[0]?.thumb} alt="" />}
             </div>
           </div>
         )}
