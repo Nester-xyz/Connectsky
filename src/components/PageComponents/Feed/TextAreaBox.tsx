c8658300c9feaafb2ad64850ae91e1aa7a4fe3e4
whoisanku <newmew.ankit@gmail.com>
2023-05-16 22:28:41 +0545

    unused code removal

--- src/components/PageComponents/Feed/TextAreaBox.tsx
+++ src/components/PageComponents/Feed/TextAreaBox.tsx
@@ -1,6 +1,5 @@
-import React, { useState, useContext, useEffect, useRef } from "react";
+import React, { useContext, useEffect, useRef } from "react";
 import { appContext } from "../../../context/appContext";
-import { useDebounce } from "use-debounce";
 
 type Props = {
   showImage: boolean;
@@ -22,35 +21,18 @@ const TextAreaBox = ({ showImage, imgUpload }: Props) => {
 
   const textBox = useRef<HTMLTextAreaElement>(null);
 
-  const [postTextLocal, setPostTextLocal] = useState("");
-  const [debouncedPostText] = useDebounce(postTextLocal, 1000);
-  const [cursorPosition, setCursorPosition] = useState(0);
-
   const randomPlacholder =
     arrayOfRandomPlaceholders[
       Math.floor(Math.random() * arrayOfRandomPlaceholders.length)
     ];
-  useEffect(() => {
-    setPostText(debouncedPostText);
-  }, [debouncedPostText]);
 
   const handlePost = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
     setPostText(e.target.value);
-    setCursorPosition(e.target.selectionStart);
-    console.log(e.target.selectionStart);
   };
 
   useEffect(() => {
     const textBoxReact = textBox.current?.selectionStart;
     console.log(textBoxReact + "textBox");
-
-    // console.log(textBoxRect.left, textBoxRect.top, textBoxRect.height);
-
-    // style.position = "absolute";
-    // style.left = `${textBoxReact}`;
-    // style.top = `${
-    //   textBoxRect.top + textBoxRect.height + textBox.current.clientTop
-    // }px`;
   }, []);
 
   return (
@@ -64,10 +46,6 @@ const TextAreaBox = ({ showImage, imgUpload }: Props) => {
           className="w-full h-40 resize-none px-4 py-2 rounded-xl focus:outline-none"
           onChange={handlePost}
           value={postText}
-          onClick={(e) => {
-            const target = e.target as HTMLTextAreaElement;
-            setCursorPosition(target.selectionStart);
-          }}
         ></textarea>
 
         {showImage && (
